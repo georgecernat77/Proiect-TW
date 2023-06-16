e4b7d1e177574fe79bf5eb08aac4ed8c9f5210d4
georgecernat77 <100781618+georgecernat77@users.noreply.github.com>
2023-06-16 13:09:55 +0300

    Add files via upload

---
+++ session/session.php
@@ -0,0 +1,18 @@
+<?php
+session_start();
+if (!isset($_SESSION["user"])) {
+    header("Location: login.php");
+}
+$expirationTime = 3600 * 24;
+if (!isset($_SESSION['created'])) {
+    $_SESSION['created'] = time();
+  } else if (time() - $_SESSION['created'] > $expirationTime) {
+    // Session has expired, destroy the session and log the user out
+    session_unset();
+    session_destroy();
+    // Redirect the user to the login page or display a message
+    header("Location: login.php");
+    exit;
+    }
+
+?>
