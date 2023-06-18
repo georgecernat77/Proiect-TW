483eea6ffeaa04bafd2005d8f98ef688cedddf88
georgecernat77 <100781618+georgecernat77@users.noreply.github.com>
2023-06-19 02:29:40 +0300

    fixed session

--- session/session.php
+++ session/session.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
 if (!isset($_SESSION["user"])) {
-    header("Location: login.php");
+    header("Location: ../login/login.php");
 }
 $expirationTime = 3600 * 24;
 if (!isset($_SESSION['created'])) {
