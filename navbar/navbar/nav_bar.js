964771c774f4a9090f1931e81d09bf3b5309fbd6
georgecernat77 <100781618+georgecernat77@users.noreply.github.com>
2023-06-19 02:27:14 +0300

    fixed appearance

---
+++ navbar/navbar/nav_bar.js
@@ -0,0 +1,14 @@
+// sidebar open close 
+let navLinks = document.querySelector(".nav-links");
+let menuOpenBtn = document.querySelector(".navbar .bx-menu");
+let menuCloseBtn = document.querySelector(".nav-links .bx-x");
+menuOpenBtn.onclick = function() {
+navLinks.style.left = "0";
+}
+menuCloseBtn.onclick = function() {
+navLinks.style.left = "-100%";
+}
+
+
+
+
