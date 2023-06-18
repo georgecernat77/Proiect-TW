40e014d5cf3817334bc4d43726b483ca2508e29f
Popa Ioana <79106550+popaioanap@users.noreply.github.com>
2023-06-19 02:21:06 +0300

    Implemented index responsive

---
+++ index/index.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href='https://unpkg.com/boxicons@2.0.7/css/boxicons.min.css' rel='stylesheet'>
+    <link rel="stylesheet" href="index.css">
+    <link rel="stylesheet" href="../navbar/nav_bar.css">
+    <title>ZooConnect</title>
+</head>
+  <body>
+  <nav>
+        <div class="navbar">
+            <i class='bx bx-menu'></i>
+            <div class="logo"><a href="../index/index.php">ZooConnect</a></div>
+            <div class="nav-links">
+                <div class="sidebar-logo">
+                    <span class="logo-name">ZooConnect</span>
+                    <i class='bx bx-x'></i>
+                </div>
+                <ul class="links">
+                    <li><a href="../index/index.php">HOME</a></li>
+                    <li><a href="../animals/animals.php">ANIMALS</a></li>
+                    <li><a href="../myaccount/myaccount.php">MY ACCOUNT</a></li>
+                    <li><a href="../visitUs/visit.php">VISIT US</a></li>
+                    <li><a href="../about/about.php">ABOUT US</a></li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+
+    <script src="../navbar/nav_bar.js"></script>
+
+    <div class="content">
+      <div class="special">
+        <img src="../images/home.png" class="special_image" alt="Special Image">
+      </div>
+
+      <div class="description-text">
+        <h1>
+          Hello!
+        </h1>
+        <h3>Are you ready </h3><h3>to discover our </h3><h3>world?</h3>
+        <h4>
+          Our zoo is the perfect place to spend time with your family.
+        </h4>
+        <h5>
+          <a href="../animals/animals.php" class="button">Get started!</a>
+        </h5>
+      </div>
+    </div>
+  </body>
+</html>
